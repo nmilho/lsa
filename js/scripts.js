989e52bb29ef288b417df7338d7322ff5b7542cc
Nuno Milho - ULSBA <20344@ulsba.min-saude.pt>
2018-03-27 15:58:52 +0100

    [WIP] contactform

--- js/scripts.js
+++ js/scripts.js
@@ -38,21 +38,64 @@ $('a[href*="#"].smooth-scroll')
 
 
 
-
 $('.clients-carousel').owlCarousel({
-      items: 5,
-      autoPlay: true,
-      pagination: false
-    });
+    items: 5,
+    autoPlay: true,
+    pagination: false
+});
+
+
+
 
 $(window).bind('scroll', function(){
-      var offset = $(document).scrollTop();
+    var offset = $(document).scrollTop();
 
-      if (offset >= 200) {
+    if (offset >= 200) {
         //$('.st-navbar').addClass("st-navbar-mini");
         $('.backtotop').removeClass("d-none");
-      } else if (offset <= 199) {
+    } else if (offset <= 199) {
         //$('.st-navbar').removeClass("st-navbar-mini");
         $('.backtotop').addClass("d-none");
-      }
-  });
+    }
+});
+
+
+
+//scrollSpy function
+function scrollSpy() {
+    var sections = ['body', 'about', 'transfers', 'tours', 'contacts'];
+    var current;
+    for (var i = 0; i < sections.length; i++) {
+        if ( $('#'+sections[i]).offset().top <= ($(window).scrollTop() + 200) ) {
+            current = sections[i];
+        }
+    }
+
+  
+
+    var activelink = $("a[href='#"+current+"']");
+
+    
+
+    $("a").not("a[href='#"+current+"']").parent().removeClass('active');
+    activelink.parent().addClass('active');
+    //$("nav-item > a[href='#"+current+"']").addClass('active');
+    //$("nav-item > a").not("a[href='#"+current+"']").removeClass('active');
+}
+
+// smooth scrolling navigation
+$("nav-item a").click( function() {
+    var target = $(this).attr("href");
+    $("body, html").animate({
+        scrollTop: $(target).offset().top
+    }, 300);
+    return false;
+});
+
+//scrollSpy call
+$(document).ready( function() {
+    scrollSpy();
+});
+$(window).scroll( function() {
+    scrollSpy();
+});
---
+++ sendcontactemail.php
@@ -0,0 +1,1 @@
+http://blog.teamtreehouse.com/create-ajax-contact-form
