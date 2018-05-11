13159cc998f90eb3c14983fd90fce1b43cccdfef
Nuno Milho - ULSBA <20344@ulsba.min-saude.pt>
2018-05-11 16:10:45 +0100

    [WIP] Tours culturais

--- pt/tours/3.php
+++ pt/tours/3.php
@@ -718,28 +718,7 @@
 
     
 
-<!-- Modal -->
-<div class="modal fade" id="tourbeja" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-upper" id="exampleModalLabel">Tour por Beja</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                
-                
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-blue" data-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- ./Modal -->
 
 
 <!-- /Start your project here-->
